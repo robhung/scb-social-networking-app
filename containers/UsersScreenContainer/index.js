8461bd36362d8737eb0923703fbec7271a221b2c
Robert Hung <rph.hung@gmail.com>
2019-02-11 08:45:35 +0800

    Improve UsersScreen and UsersScreenContainer

---
+++ containers/UsersScreenContainer/index.js
@@ -0,0 +1,39 @@
+import { compose, lifecycle, withState, withHandlers } from "recompose";
+
+const initialState = {
+  loading: true,
+  error: false,
+  users: []
+};
+
+const handlers = {
+  onFetch: ({ state, updateState }) => async () => {
+    updateState({ ...state, loading: true });
+
+    await fetch("https://jsonplaceholder.typicode.com/users")
+      .then(response => response.json())
+      .then(responseJson => {
+        updateState({
+          ...state,
+          loading: false,
+          users: responseJson
+        });
+      })
+      .catch(error => {
+        updateState({ ...state, loading: false, error });
+      });
+  },
+  onUser: ({ navigation }) => userId => navigation.navigate("Main", { userId })
+};
+
+const UsersScreenContainer = compose(
+  withState("state", "updateState", initialState),
+  withHandlers(handlers),
+  lifecycle({
+    componentDidMount() {
+      this.props.onFetch();
+    }
+  })
+);
+
+export default UsersScreenContainer;
---
+++ screens/UsersScreen/index.js
@@ -0,0 +1,59 @@
+import React from "react";
+import { Button, FlatList, StyleSheet, Text, View } from "react-native";
+
+import UsersScreenContainer from "../../containers/UsersScreenContainer";
+
+import ErrorMessage from "../../components/ErrorMessage";
+import Loading from "../../components/Loading";
+import User from "../../components/User";
+
+const UsersScreen = ({ onFetch, onUser, state }) => (
+  <View style={styles.wrapper}>
+    {(() => {
+      if (state.loading) return <Loading flex={1} />;
+      if (state.error)
+        return (
+          <React.Fragment>
+            <ErrorMessage
+              containerStyle={styles.errorWrapper}
+              text="Unable to retrieve User data."
+            />
+            <Button onPress={onFetch} title="Refetch" color="#000" />
+          </React.Fragment>
+        );
+
+      return (
+        <FlatList
+          style={styles.usersWrapper}
+          contentContainerStyle={styles.usersContent}
+          numColumns={2}
+          data={state.users}
+          showsVerticalScrollIndicator={false}
+          keyExtractor={item => `${item.id}`}
+          onRefresh={() => onFetch()}
+          refreshing={state.loading}
+          renderItem={({ item }) => (
+            <User id={item.id} name={item.name} onUser={onUser} />
+          )}
+        />
+      );
+    })()}
+  </View>
+);
+
+const styles = StyleSheet.create({
+  wrapper: {
+    flex: 1
+  },
+  usersWrapper: {},
+  usersContent: {
+    alignItems: "center",
+    paddingTop: 12
+  },
+  user: {
+    height: 50,
+    borderWidth: 1
+  }
+});
+
+export default UsersScreenContainer(UsersScreen);

00f0012b2595a1c60ada1bbc51e583d59e5bca5c
Robert Hung <rph.hung@gmail.com>
2019-02-11 22:29:42 +0800

    Improve AlbumsScreen and create container

--- containers/AlbumsScreenContainer/index.js
+++ containers/AlbumsScreenContainer/index.js
@@ -1,9 +1,36 @@
 import { compose, lifecycle, withState, withHandlers } from "recompose";
 import { connect } from "react-redux";
 
-const initialState = {};
+// import { userSelected } from "../../data/redux/actions/user";
 
-const handlers = {};
+const initialState = {
+  loading: true,
+  error: false,
+  albums: []
+};
+
+const handlers = {
+  onFetch: ({ state, updateState, user }) => async () => {
+    updateState({ ...state, loading: true });
+
+    await fetch(`https://jsonplaceholder.typicode.com/albums?userId=${user.id}`)
+      .then(response => response.json())
+      .then(responseJson => {
+        updateState({
+          ...state,
+          loading: false,
+          albums: responseJson
+        });
+      })
+      .catch(error => {
+        updateState({ ...state, loading: false, error });
+      });
+  },
+  onAlbum: ({ dispatch, navigation }) => post => {
+    // dispatch(userSelected(user));
+    navigation.navigate("PostComments", { post });
+  }
+};
 
 const AlbumsScreenContainer = compose(
   connect(({ user }) => ({ user })),
@@ -12,6 +39,7 @@ const AlbumsScreenContainer = compose(
   lifecycle({
     componentDidMount() {
       this.props.navigation.setParams({ user: this.props.user });
+      this.props.onFetch();
     }
   })
 );
--- screens/AlbumsScreen/index.js
+++ screens/AlbumsScreen/index.js
@@ -1,22 +1,60 @@
 import React from "react";
-import { Button, ScrollView, StyleSheet } from "react-native";
-import { ExpoLinksView } from "@expo/samples";
+import { Button, FlatList, StyleSheet, View } from "react-native";
 
-import AlbumsScreenContainer from '../../containers/AlbumsScreenContainer';
+import AlbumsScreenContainer from "../../containers/AlbumsScreenContainer";
 
-const AlbumsScreen = () => (
-  <ScrollView style={styles.container}>
-    {/* Go ahead and delete ExpoLinksView and replace it with your
-           * content, we just wanted to provide you with some helpful links */}
-    <ExpoLinksView />
-  </ScrollView>
+import ErrorMessage from "../../components/ErrorMessage";
+import Loading from "../../components/Loading";
+import Album from "../../components/Album";
+
+const AlbumsScreen = ({ onFetch, onAlbum, state }) => (
+  <View style={styles.wrapper}>
+    {(() => {
+      if (state.loading) return <Loading flex={1} />;
+      if (state.error)
+        return (
+          <View style={styles.errorWrapper}>
+            <ErrorMessage
+              textStyle={styles.errorText}
+              text="Unable to retrieve Albums, please refetch."
+            />
+            <Button onPress={onFetch} title="Refetch" color="#000" />
+          </View>
+        );
+
+      return (
+        <FlatList
+          style={styles.albumsWrapper}
+          contentContainerStyle={styles.albumsContent}
+          numColumns={2}
+          data={state.albums}
+          showsVerticalScrollIndicator={false}
+          keyExtractor={item => `${item.id}`}
+          onRefresh={() => onFetch()}
+          refreshing={state.loading}
+          renderItem={({ item }) => <Album album={item} onAlbum={onAlbum} />}
+        />
+      );
+    })()}
+  </View>
 );
 
 const styles = StyleSheet.create({
-  container: {
+  wrapper: {
+    flex: 1
+  },
+  albumsWrapper: {},
+  albumsContent: {
+    alignItems: "center",
+    paddingVertical: 12
+  },
+  errorWrapper: {
     flex: 1,
-    paddingTop: 15,
-    backgroundColor: "#fff"
+    justifyContent: "center",
+    alignItems: "center"
+  },
+  errorText: {
+    fontSize: 18
   }
 });
 

1c5995b18e2144afe0835d35600a85460bb43b53
Robert Hung <rph.hung@gmail.com>
2019-02-11 22:09:36 +0800

    Create Post

---
+++ components/Post/index.js
@@ -0,0 +1,28 @@
+import React from "react";
+import PropTypes from "prop-types";
+import { StyleSheet, Text, TouchableOpacity } from "react-native";
+
+const Post = ({ post, onPost }) => (
+  <TouchableOpacity style={styles.wrapper} onPress={() => onPost(post)}>
+    <Text>{post.title}</Text>
+    <Text>{post.body}</Text>
+  </TouchableOpacity>
+);
+
+const styles = StyleSheet.create({
+  wrapper: {
+    borderWidth: 1,
+    marginVertical: 12,
+    marginHorizontal: 18,
+    alignItems: "center"
+  }
+});
+
+Post.propTypes = {
+  post: PropTypes.objectOf(
+    PropTypes.oneOfType([PropTypes.number, PropTypes.string])
+  ).isRequired,
+  onPost: PropTypes.func.isRequired
+};
+
+export default Post;
